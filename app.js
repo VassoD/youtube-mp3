38184fae0f9d136c1cbd389353016bbb07ee4d9d
Vasiliki Doropoulou <vasiliki@ublo.immo>
2023-09-23 19:44:02 +0200

    wip: keep history

--- app.js
+++ app.js
@@ -1,61 +1,91 @@
-// Required packages
 const express = require("express");
-// npm install node-fetch@2
 const fetch = require("node-fetch");
-require('dotenv').config()
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
 
-// Create express server
 const app = express();
 
-// Indicate the port number server will run on
 const PORT = process.env.PORT || 3000;
 
-// Set template engine
 app.set("view engine", "ejs");
-app.use(express.static('public'));
-
-// Needed to parse html data for POST requests
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// GET route
+// Load the search history from the JSON file
+let searchHistory = [];
+
+try {
+  const searchHistoryData = fs.readFileSync(
+    path.join(__dirname, "search-history.json"),
+    "utf-8"
+  );
+  searchHistory = JSON.parse(searchHistoryData);
+} catch (err) {
+  console.error("Error reading search history:", err);
+}
+
+// GET route for rendering the search history
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { searchHistory });
 });
 
 // POST route
 app.post("/convert-mp3", async (req, res) => {
-
   const videoId = req.body.videoId;
-  
-  if(
-    videoId === undefined ||
-    videoId === "" ||
-    videoId === null
-  ){
-    return res.render("index", { success : false, message : "Please enter a video ID (ID starts after = in the video's link)"});
-  } else {
-    
-    const fetchAPI = await fetch(`https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}`, {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-key": process.env.API_KEY,
-        "x-rapidapi-host": process.env.API_HOST
-        }
+
+  if (videoId === undefined || videoId === "" || videoId === null) {
+    return res.render("index", {
+      success: false,
+      message:
+        "Please enter a video ID (ID starts after = in the video's link)",
     });
+  } else {
+    const fetchAPI = await fetch(
+      `https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}`,
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-key": process.env.API_KEY,
+          "x-rapidapi-host": process.env.API_HOST,
+        },
+      }
+    );
 
     const fetchResponse = await fetchAPI.json();
- 
-    if(fetchResponse.status === "ok")
-      return res.render("index",{ success : true,  song_title : fetchResponse.title, song_link : fetchResponse.link})
-    else
-      return res.render("index", { success : false, message : fetchResponse.msg});
+
+    if (fetchResponse.status === "ok") {
+      const searchEntry = {
+        videoId,
+        song_title: fetchResponse.title,
+        song_link: fetchResponse.link,
+      };
+
+      // Add the search entry to the search history
+      searchHistory.push(searchEntry);
+
+      // Save the updated search history to the JSON file
+      fs.writeFileSync(
+        path.join(__dirname, "search-history.json"),
+        JSON.stringify(searchHistory, null, 2),
+        "utf-8"
+      );
+
+      return res.render("index", {
+        success: true,
+        song_title: fetchResponse.title,
+        song_link: fetchResponse.link,
+      });
+    } else {
+      return res.render("index", {
+        success: false,
+        message: fetchResponse.msg,
+      });
+    }
   }
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
